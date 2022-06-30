8d7a31332737db811c367c5f5dff950d37488432
alexsandr1945 <alexsander1945@gmail.com>
2022-06-30 13:39:27 +0300

    Добавлены материалы по работе со списками

---
+++ Lesson_6/sort_array.py
@@ -0,0 +1,76 @@
+def insert_sort(A):
+    """
+    Сортировка списка А методом вставок.
+    :param A: функции передаётся сортируемый список.
+    :return: переданный список отсортирован.
+    """
+
+    N = len(A)
+    for top in range(1, N):
+        k = top
+        while k > 0 and A[k-1] > A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k -=1
+
+
+def choise_sort(A):
+    """
+    Сортировка списка А методом выбора.
+    :param A: функции передаётся спиоок А.
+    :return: переданный список отсортирован.
+    """
+
+    N = len(A)
+    for pos in range(0,N-1):
+        for k in range(pos+1, N):
+            if A[k] < A[pos]:
+                A[k], A[pos] = A[pos], A[k]
+
+
+
+def bubble_sort(A):
+    """
+    Сортировка списка А методом пузырька.
+    :param A: функции передаётся список А.
+    :return: переданный список отсортирован
+    """
+
+    N = len(A)
+    for bypass in range (1, N):
+        for k in range(0, N-bypass):
+            if A[k] > A[k+1]:
+                A[k], A[k+1] = A[k+1], A[k]
+
+
+def test_sort(sort_algorithm):
+    """
+    Проверяет алгоритмы сортировки
+    :param sort_algorithm:
+    :return: результаты проверки.
+    """
+
+    print("Тестируем : ", sort_algorithm.__doc__)
+    print('testcase #1: ', end='')
+    A = [4, 2, 5, 1, 3]
+    A_sorted = [1, 2, 3, 4, 5]
+    sort_algorithm(A)
+    print("Тест пройден" if A == A_sorted else 'Тест провален')
+
+    print('testcase #2: ', end='')
+    A = list(range(10, 20)) + list(range(0, 10))
+    A_sorted = list(range(20))
+    sort_algorithm(A)
+    print("Тест пройден" if A == A_sorted else 'Тест провален')
+
+    print('testcase #3: ', end='')
+    A = [4, 2, 4, 2, 1]
+    A_sorted = [1, 2, 2, 4, 4]
+    sort_algorithm(A)
+    print("Тест пройден" if A == A_sorted else 'Тест провален')
+
+
+if __name__ == "__main__":
+    test_sort(insert_sort)
+    test_sort(insert_sort)
+    test_sort(bubble_sort)
+
